22bd95f2b4f0bdbc25311a319068628e4cc7afef
Vladimir varnavskiy <var@33kita.ru>
2023-01-24 11:48:13 +0300

    Сделал проверку баланса счёта Голока-феста

---
+++ BudgetUpdaterApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sankirtana.Web.Common;
+using TinkoffBusiness.Api.Client;
+
+public class Program
+{
+    private static EnvironmentConfig _environmentConfig;
+    
+    static async Task Main(string[] args)
+    {
+        ConfigureServices();
+        
+        Console.WriteLine("API URL: " + _environmentConfig.TinkoffApiSettings.ApiUrl);
+
+        var tinkoffApiClient = new TinkoffApiClient(_environmentConfig.TinkoffApiSettings);
+        var accounts = await tinkoffApiClient.GetAccountsAsync();
+        Console.WriteLine("Accounts count: " + accounts.Count);
+        if (accounts.Any())
+        {
+            var glkAccount = accounts.FirstOrDefault(a => a.Name.ToLower().Contains("голока"));
+            Console.WriteLine($"Собрано: {glkAccount.Balance.Otb}");
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void ConfigureServices()
+    {
+        IServiceCollection serviceCollection = new ServiceCollection();
+
+        var envConfig = new EnvironmentConfig();
+        serviceCollection.AddSingleton(envConfig);
+
+        _environmentConfig = envConfig;
+    }
+}
